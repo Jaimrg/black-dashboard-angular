a3b15bbf08c721ddeae934ba991791e90d830513
Jaime RG <75501289+Jaimrg@users.noreply.github.com>
2023-01-25 06:06:37 +0200

    general changes

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -15,55 +15,55 @@ export const ROUTES: RouteInfo[] = [
     icon: "icon-chart-pie-36",
     class: ""
   },
-  {
-    path: "/icons",
-    title: "Icons",
-    rtlTitle: "الرموز",
-    icon: "icon-atom",
-    class: ""
-  },
-  {
-    path: "/maps",
-    title: "Maps",
-    rtlTitle: "خرائط",
-    icon: "icon-pin",
-    class: "" },
-  {
-    path: "/notifications",
-    title: "Notifications",
-    rtlTitle: "إخطارات",
-    icon: "icon-bell-55",
-    class: ""
-  },
+  // {
+  //   path: "/icons",
+  //   title: "Icons",
+  //   rtlTitle: "الرموز",
+  //   icon: "icon-atom",
+  //   class: ""
+  // },
+  // {
+  //   path: "/maps",
+  //   title: "Maps",
+  //   rtlTitle: "خرائط",
+  //   icon: "icon-pin",
+  //   class: "" },
+  // {
+  //   path: "/notifications",
+  //   title: "Notifications",
+  //   rtlTitle:"",
+  //   icon: "icon-bell-55",
+  //   class: ""
+  // },
 
   {
     path: "/user",
-    title: "User Profile",
-    rtlTitle: "ملف تعريفي للمستخدم",
+    title: "Cadastro de Alunos",
+    rtlTitle: "",
     icon: "icon-single-02",
     class: ""
   },
   {
     path: "/tables",
-    title: "Table List",
-    rtlTitle: "قائمة الجدول",
+    title: "Lista de Alunos",
+    rtlTitle: "",
     icon: "icon-puzzle-10",
     class: ""
   },
-  {
-    path: "/typography",
-    title: "Typography",
-    rtlTitle: "طباعة",
-    icon: "icon-align-center",
-    class: ""
-  },
-  {
-    path: "/rtl",
-    title: "RTL Support",
-    rtlTitle: "ار تي ال",
-    icon: "icon-world",
-    class: ""
-  }
+  // {
+  //   path: "/typography",
+  //   title: "Typography",
+  //   rtlTitle: "طباعة",
+  //   icon: "icon-align-center",
+  //   class: ""
+  // },
+  // {
+  //   path: "/rtl",
+  //   title: "RTL Support",
+  //   rtlTitle: "ار تي ال",
+  //   icon: "icon-world",
+  //   class: ""
+  // }
 ];
 
 @Component({
